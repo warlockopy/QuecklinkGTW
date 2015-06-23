6f3ff2a5528bd041d0f6ee4bafa4813beefb6d69
Hugo <crazydarkmaster@gmai.com>
2015-06-23 08:47:08 -0400

    analiza codigo de Error 200 del servicio http

---
+++ Queclink/src/main/java/server/HttpOutput.java
@@ -0,0 +1,21 @@
+package server;
+
+public class HttpOutput {
+	
+	private int code;
+	private String output;
+	
+	public HttpOutput (int code, final String output){
+		this.code = code;
+		this.output = output;
+	}
+	
+	public int getCode (){
+		return code;
+	}
+	
+	public String getOutput (){
+		return output;
+	}
+	
+}
